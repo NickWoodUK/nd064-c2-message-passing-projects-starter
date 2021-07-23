81bdd202aa28cd5561d57422f91e1d6d92b98e11
NickWoodUK <nickwood@gmail.com>
2021-07-22 21:38:01 -0500

    refactor location api

--- modules/frontend/src/components/Connection.js
+++ modules/frontend/src/components/Connection.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class Connection extends Component {
   constructor(props) {
--- modules/frontend/src/components/Persons.js
+++ modules/frontend/src/components/Persons.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Connection from "./Connection";
 
 class Persons extends Component {
