ce151ffc266663210e679c806d41fe61c5962946
NickWoodUK <nickwood@gmail.com>
2021-08-04 22:04:22 -0500

    split location api into stand alone service

--- modules/locationApi/app/udaconnect/__init__.py
+++ modules/locationApi/app/udaconnect/__init__.py
@@ -1,8 +1,8 @@
-from app.udaconnect.models import Connection, Location, Person  # noqa
-from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema  # noqa
+from modules.locationApi.app.udaconnect.models import Location
+from modules.locationApi.app.udaconnect.schemas import LocationSchema
 
 
 def register_routes(api, app, root="api"):
-    from app.udaconnect.controllers import api as udaconnect_api
+    from modules.locationApi.app.udaconnect.controllers import api as udaconnect_api
 
     api.add_namespace(udaconnect_api, path=f"/{root}")
